c5c907c868ae5297cdadc973bca2b81425b6e41b
Kevin Caliboso <calibosokevin@gmail.com>
2025-05-29 15:57:42 +0800

    refactor: changed button and font sizes on ErrorPage

--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { isRouteErrorResponse, Link, useRouteError } from "react-router";
 
 const ErrorPage = () => {
@@ -7,9 +8,11 @@ const ErrorPage = () => {
     return (
       <div className="flex h-dvh w-full items-center justify-center">
         <div className="space-y-4 text-center">
-          <h1 className="text-4xl font-bold">404 - Error</h1>
-          <p>The page doesn&apos;t exist.</p>
-          <Link to="/">Back to Home</Link>
+          <h1 className="text-6xl font-extrabold">404 - Error</h1>
+          <p className="text-2xl">The page doesn&apos;t exist.</p>
+          <Button>
+            <Link to="/">Back to Home</Link>
+          </Button>
         </div>
       </div>
     );
