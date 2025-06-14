10f41b1ff275904265f29ead5813a51b2c08c8b7
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-14 10:28:57 +0800

    fix: styling on hero component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <div className="flex flex-1 h-full flex-col py-36 justify-center items-center gap-6">
-      <div className="text-8xl font-extrabold text-center overflow-hidden will-change-transform">
+    <div className="flex flex-1 h-full flex-col py-4 lg:py-36 justify-center items-center gap-6">
+      <div className="text-6xl lg:text-8xl font-extrabold text-center overflow-hidden will-change-transform">
         <div className="overflow-hidden">
           <p className="hero-text">
             Made by <span className="text-primary">Food Lovers.</span>
@@ -14,7 +14,7 @@ export default function Hero() {
           <p className="hero-text">Shared with the World.</p>
         </div>
       </div>
-      <Button className="cursor-pointer font-bold text-xl px-6 py-6 opacity-0 hero-button">
+      <Button className="cursor-pointer font-bold text-xl px-6 py-6 hero-button">
         <Link to="/auth/login">Join as a Creator</Link>
       </Button>
     </div>
