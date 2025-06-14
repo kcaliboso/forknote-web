af9eefb35e7faacf6f59bf61fadd24455db14942
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-14 10:27:18 +0800

    fix: styling on page-section component

--- src/components/sections/page-section.tsx
+++ src/components/sections/page-section.tsx
@@ -15,11 +15,15 @@ const PageSection: React.FC<PropsWithChildren<PageSectionProps>> = ({
   return (
     <div
       className={cn(
-        "flex flex-col py-4 px-4 sm:px-6 md:px-8 lg:px-60 lg:py-6 ",
+        "h-screen flex flex-col justify-center py-4 px-4 sm:px-6 md:px-8 xl:px-60 lg:py-6 ",
         className
       )}
     >
-      <div className={cn("flex-1 flex flex-col h-full", sx)}>{children}</div>
+      <div
+        className={cn("flex flex-col gap-8 lg:gap-2 h-[calc(100vh-64px)]", sx)}
+      >
+        {children}
+      </div>
     </div>
   );
 };
