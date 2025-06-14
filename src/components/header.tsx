4bc2a645fd3a09d2e17191f77c05c2111f9b046a
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-14 10:26:49 +0800

    fix: styling on header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,25 +12,27 @@ export default function Header() {
   return (
     <div
       id="header"
-      className="w-full py-2 flex items-center justify-between px-2"
+      className="w-full py-2 flex items-center justify-between px-2 h-16"
     >
       <Link to="/" className="font-bold text-4xl">
         Fork<span className="text-primary">Note</span>
       </Link>
-      <Navbar />
-      <div className="flex gap-4 items-center">
-        <Search />
-        <ModeToggle />
-        {/* TODO: User avatar here */}
-        {user ? (
-          "User Avatar"
-        ) : (
-          <>
-            <Link to="/auth">
-              <Button className="cursor-pointer">Login</Button>
-            </Link>
-          </>
-        )}
+      <div className="hidden xl:flex w-full justify-end gap-12">
+        <Navbar />
+        <div className="flex gap-4 items-center">
+          <Search />
+          <ModeToggle />
+          {/* TODO: User avatar here */}
+          {user ? (
+            "User Avatar"
+          ) : (
+            <>
+              <Link to="/auth">
+                <Button className="cursor-pointer">Login</Button>
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
