74285f7dd01f89865c7d80d8390cd82253f9e464
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-19 10:09:51 +0800

    feat: added image url and markup creation utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import DOMPurify from "dompurify";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function createImageUrl(imageLink: string, path: string) {
+  return `${import.meta.env.VITE_API_URL}/uploads/images/${path}/${imageLink}`;
+}
+
+export function createMarkup(htmlString: string) {
+  return {
+    __html: DOMPurify.sanitize(htmlString),
+  };
 }
