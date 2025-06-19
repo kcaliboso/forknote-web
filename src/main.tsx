489c4ff921a75c046b94152e6c2668df5a0bacc8
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-19 10:03:41 +0800

    feat: added tanstack query on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,16 @@ import { router } from "./router.tsx";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
 );
