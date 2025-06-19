3491e359aa9b4d9980f58c48c82da7aae0993771
Kevin Caliboso <calibosokevin@gmail.com>
2025-06-19 10:05:26 +0800

    feat: hide footer while on dashboard

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,18 @@
+import { cn } from "@/lib/utils";
 import HeartIcon from "./icons/heart-icon";
+import { useLocation } from "react-router";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear().toString();
+  const location = useLocation();
+
   return (
-    <div className="flex gap-2 items-center w-full justify-center text-sm tracking-wide ">
+    <div
+      className={cn(
+        "flex gap-2 items-center w-full justify-center text-sm tracking-wide",
+        location.pathname.includes("my") ? "hidden" : "visible"
+      )}
+    >
       Developed with
       <HeartIcon className="size-8 fill-primary" />|{" "}
       <p>Copyright &copy; {currentYear}</p>
